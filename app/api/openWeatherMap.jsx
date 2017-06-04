9b198ef6cfafec0a88b0056e8fad773b1618e92b
Ravi Teja <raviteja@Ravis-MacBook-Pro.local>
2017-06-04 18:32:05 +0530

    initial commit - weather app

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/find?appid=d83f3e7ee9002bab7ce3887541dfb5f6&units=metric';
+
+module.exports = {
+  getTemp : function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        return res.data.list[0].main.temp;
+      } else {
+        throw new Error(res.data.message);
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+};
