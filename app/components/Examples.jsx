26a3ef480294dd0046a6534bf5d87d592d8274a2
Ravi Teja <raviteja@Ravis-MacBook-Pro.local>
2017-06-04 21:36:14 +0530

    Added styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function () {
@@ -12,7 +13,18 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Bangalore">Bangalore Weather</Link>
+        </li>
+        <li>
+          <Link to="/?location=Chennai">Chennai Weather</Link>
+        </li>
+      </ol>
+    </div>
   );
 }
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,33 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-  return (
-    <div>
-      <h2>Nav Component</h2>
-      <IndexLink to="/" activeClassName='active' activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName='active' activeStyle={{fontWeight:'bold'}}>About</Link>
-      <Link to="/examples" activeClassName='active' activeStyle={{fontWeight:'bold'}}>Examples</Link>
-    </div>
-  );
-}
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="dropdown menu" data-dropdown-menu>
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeclassNameName='active' activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeclassNameName='active' activeStyle={{fontWeight:'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeclassNameName='active' activeStyle={{fontWeight:'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li><input type="search" placeholder="Search" /></li>
+            <li><button type="button" className="button">Search</button></li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+});
 
 module.exports = Nav;
