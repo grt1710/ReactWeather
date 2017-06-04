b163ce906e2694bc0368160336c6a136116aa946
Ravi Teja <raviteja@Ravis-MacBook-Pro.local>
2017-06-04 18:51:55 +0530

    changed to {}

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,6 +10,10 @@ var React = require('react');
 //   }
 // });
 
-var Examples = (props) => <h3>Examples Component</h3>
+var Examples = (props) => {
+  return (
+    <h3>Examples Component</h3>
+  );
+}
 
 module.exports = Examples;
